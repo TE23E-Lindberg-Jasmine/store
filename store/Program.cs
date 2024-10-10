0ac7a4a8b273a8cd4682574cfaf393a6f1e7ef58
jasmine lindberg <Jasmine.lindberg@elev.ga.ntig.se>
2024-10-10 12:37:38 +0200

    first commit

---
+++ store/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+int ageNum = 0;
+bool success = false;
+
+{
+while (success == false)
+Console.WriteLine("Hur gammal är du");
+String age = Console.ReadLine();
+
+success = int.TryParse(age, out ageNum);
+}
+if(success == false)
+{
+    Console.WriteLine("du måste skriva en siffra");
+
+}
+
+
+if (ageNum < 18)
+{
+    Console.WriteLine("du är för ung");
+}
+
+else {
+    Console.WriteLine("wow du är vuxen");
+}
+
+Console.ReadLine();
