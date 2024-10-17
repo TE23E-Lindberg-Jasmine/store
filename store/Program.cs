4dd65011b45d5630a600b2e0cdf4e00fcd8e50ac
jasmine lindberg <Jasmine.lindberg@elev.ga.ntig.se>
2024-10-17 13:15:57 +0200

    jijijpj

--- store/Program.cs
+++ store/Program.cs
@@ -1,29 +1,46 @@
-﻿
+﻿int i = 0;
 
-int ageNum = 0;
-bool success = false;
 
+while( i < 10)
 {
-while (success == false)
-Console.WriteLine("Hur gammal är du");
-String age = Console.ReadLine();
-
-success = int.TryParse(age, out ageNum);
+Console.WriteLine("hej");
+i++;
 }
-if(success == false)
+
+int num = 996;
+
+for(int j = 0; j <10; j++)
 {
-    Console.WriteLine("du måste skriva en siffra");
+
 
 }
 
 
-if (ageNum < 18)
-{
-    Console.WriteLine("du är för ung");
-}
 
-else {
-    Console.WriteLine("wow du är vuxen");
-}
 
-Console.ReadLine();
+
+// int ageNum = 0;
+// bool success = false;
+
+// while (success == false)
+// Console.WriteLine("Hur gammal är du");
+// String age = Console.ReadLine();
+// success = int.TryParse(age, out ageNum);
+
+// if(success == false)
+// {
+//     Console.WriteLine("du måste skriva en siffra");
+
+// }
+
+
+// if (ageNum < 18)
+// {
+//     Console.WriteLine("du är för ung");
+// }
+
+// else {
+//     Console.WriteLine("wow du är vuxen");
+// }
+
+// Console.ReadLine();
